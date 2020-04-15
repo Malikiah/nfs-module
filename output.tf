65f9f1ef716e873b7685cbd321471f258a886a76
Damian McCarthy <damianmmccarthy@protonmail.com>
2020-04-15 16:18:05 -0600

    update

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "nfs_nodes" {
     value = [for droplet in digitalocean_droplet.nfs: format("${droplet.name} ansible_host=${droplet.ipv4_address}")]
 }
+
+output "nfs_domain" {
+    value = var.nfs_domain
+}
